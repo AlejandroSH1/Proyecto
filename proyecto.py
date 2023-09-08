cb7f9b0257a7e20c86d07a0a6c36c4e2ddc0b264
AlejandroSH1 <142269078+AlejandroSH1@users.noreply.github.com>
2023-09-07 21:42:24 -0600

    proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -12,7 +12,7 @@ def western(wes_1, wes_2, wes_3):
     return prom_wes
 
 def comedia(com_1, com_2, com_3):
-    prom_com = (com_1 + com_2 + com_1) / 3
+    prom_com = (com_1 + com_2 + com_3) / 3
     return prom_com
 
 def caricatura(car_1, car_2, car_3):
@@ -22,23 +22,33 @@ def caricatura(car_1, car_2, car_3):
 def ficcion(fic_1, fic_2, fic_3):
     prom_fic = (fic_1 + fic_2 + fic_3) / 3
     return prom_fic
-
-#Dependiendo de los resultados de los promedios, se va a calcular el género que más ve el usuario
 """
-Me marca error en esta parte, aún no domino bien los booleanos pero la idea es que se comparen los resultados de los 
-promedios, para de esta forma obtener el género más a fin al usuario
+Dependiendo de los resultados de los promedios, se va a calcular el género que más ve el usuario
+por medio de if y elif y se va a imprimir el promedio que se obtuvo y el género al que pertenece
 """
 def genero_preferido(genero_1, genero_2, genero_3, genero_4, genero_5):
-    if genero_1 > genero_2 and genero_1 > genero_3 and genero_1 > genero_4 and genero_1 > genero_5:
-        print("El género de suspenso predomina en tus gustos")
-    elif genero_2 > genero_1 and genero_2 > genero_3 and genero_2 > genero_4 and genero_2 > genero_5:
-        print("El género western predomina en tus gustos")
-    elif genero_3 > genero_1 and genero_3 > genero_2 and genero_3 > genero_4 and genero_3 > genero_5:
-        print("El género de comedia predomina en tus gustos")
-    elif genero_4 > genero_1 and genero_4 > genero_2 and genero_4 > genero_3 and genero_4 > genero_5:
-        print("El género caricatura predomina en tus gustos")
-    elif genero_5 > genero_1 and genero_5 > genero_2 and genero_5 > genero_3 and genero_5 > genero_4:
-        print("El género de ficción predomina en tus gustos")
+    maximo = [genero_1, "Suspenso"]
+    if genero_2 > maximo[0]:
+        maximo = [genero_2, "Western"]
+    elif genero_2 == maximo[0]:
+        maximo.append(genero_2)
+        maximo.append("Western")
+    if genero_3 > maximo[0]:
+        maximo = [genero_3, "Comedia"]
+    elif genero_3 == maximo[0]:
+        maximo.append(genero_3)
+        maximo.append("Comedia")
+    if genero_4 > maximo[0]:
+        maximo = [genero_4, "Caricatura"]
+    elif genero_4 == maximo[0]:
+        maximo.append(genero_4)
+        maximo.append("Caricatura")
+    if genero_5 > maximo[0]:
+        maximo = [genero_5, "Ficcion"]
+    elif genero_5 == maximo[0]:
+        maximo.append(genero_5)
+        maximo.append("Ficcion")
+    return maximo
 
 # Esta instrucción permite al usuario entender que hará con las películas que se le mostrarán
 print("Califica las películas sobre 10 y en caso de no haber visto alguna, coloca 0 ")
@@ -61,14 +71,10 @@ star_wars = int(input("star wars "))
 avatar  = int(input("avatar "))
 
 #En este punto se llama la función del genero preferido, para mostrarle al usuario su género más visto
-"""
-De igual forma, en esta parte cuento con error, aunque la idea es imprimir lo que obtuvimos en la función
-genero_preferido para mostrarle al usuario el género que mejor calificó
-"""
-suspenso(intriga, los_otros, sexto_sentido)
-western(django, los_ocho_mas_odiados, comancheria)
-comedia(barbie, son_como_niños, scary_movie)
-caricatura(cars, shrek, up)
-ficcion(avatar, star_wars, spiderman_2)
+gen1 = suspenso(intriga, los_otros, sexto_sentido)
+gen2 = western(django, los_ocho_mas_odiados, comancheria)
+gen3 = comedia(barbie, son_como_niños, scary_movie)
+gen4 = caricatura(cars, shrek, up)
+gen5 = ficcion(avatar, star_wars, spiderman_2)
 
-print(genero_preferido(suspenso, western, comedia, caricatura, ficcion))
+print(genero_preferido(gen1, gen2, gen3, gen4, gen5))
