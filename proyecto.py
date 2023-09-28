9b550c26243c355cb6a9d6af3771d9171a19a9e4
AlejandroSH1 <142269078+AlejandroSH1@users.noreply.github.com>
2023-09-28 01:10:21 -0600

    proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -41,22 +41,39 @@ def genero_preferido(genero_1, genero_2, genero_3, genero_4, genero_5):
     if genero_4 > maximo[0]:
         maximo = [genero_4, "Caricatura"]
     elif genero_4 == maximo[0]:
-        maximo.append(genero_4)
+        maximo.append(genero_4,)
         maximo.append("Caricatura")
     if genero_5 > maximo[0]:
         maximo = [genero_5, "Ficcion"]
     elif genero_5 == maximo[0]:
         maximo.append(genero_5)
         maximo.append("Ficcion")
-"""
-maximo.pop(), según lo que leí en W3, sirve para quitar elementos de una lista en base a un número
-que representa el orden de la lista, pero no me está sirviendo como debería, ya que si colocara 0
-me debería de quitar el primer elemento y me está quitando el 2do, de igual forma si hay más de un
-genero preferido en vez de eliminarme un elemento, se queda con un solo elemento, voy a estudiarlo
-y probarlo para entender por qué cambia sufuncionamiento a como dice en la página W3.
-"""
-    return maximo.pop(-1)
-
+        
+    if len(maximo) == 2:
+        maximo.pop(0)
+        return maximo
+    elif len(maximo) == 4:
+        maximo.pop(0)
+        maximo.pop(1)
+        return maximo
+    elif len(maximo) == 6:
+        maximo.pop(0)
+        maximo.pop(1)
+        maximo.pop(2)
+        return maximo
+    elif len(maximo) == 8:
+        maximo.pop(0)
+        maximo.pop(1)
+        maximo.pop(2)
+        maximo.pop(3)
+        return maximo
+    elif len(maximo) == 10:
+        maximo.pop(0)
+        maximo.pop(1)
+        maximo.pop(2)
+        maximo.pop(3)
+        maximo.pop(4)
+        return maximo
 
 # Esta instrucción permite al usuario entender que hará con las películas que se le mostrarán
 print("Califica las películas sobre 10 y en caso de no haber visto alguna, coloca 0 ")
@@ -86,7 +103,12 @@ gen4 = caricatura(cars, shrek, up)
 gen5 = ficcion(avatar, star_wars, spiderman_2)
 
 preferido = genero_preferido(gen1, gen2, gen3, gen4, gen5)
+print()
 print(preferido)
+print()
+
+if len(preferido) > 1:
+    preferido = [input("Ingresa el nombre del genero que quieras ver hoy: \n")]
 
 #Se crean listas con recomendaciones de películas según su género al que pertenece
 lista_suspenso = ["el_niño", "halloween", "un_lugar_en_silencio", "fragmentado", "la_purga"]
@@ -97,52 +119,57 @@ lista_ficcion = ["interestelar", "e.t.", "pantera_negra", "avengers", "the_batma
 
 #Se aplican if´s para encontrar el género preferido y mostrarle las recomendaciones al usuario
 #El usuario puede elegir si quedarse con la primera recomendación o cualquiera posterior
-if preferido =="Suspenso":
+if preferido == ["Suspenso"]:
     peli_sus = 0
     while(peli_sus < len(lista_suspenso)):
-        print("te recomendamos ver: ", lista_suspenso[peli_sus])
+        print("te recomendamos ver: \n", lista_suspenso[peli_sus])
         pregu1 = input("¿Deseas otra recomendacion? ")
         if pregu1 != "no":
             peli_sus = peli_sus + 1
         else:
             peli_sus = len(lista_suspenso)
+    print("\nNo tenemos más recomendaciones por el momento, gracias")
         
-if preferido =="Western":
+if preferido == ["Western"]:
     peli_wes = 0
     while(peli_wes < len(lista_western)):
-        print("te recomendamos ver: ", lista_western[peli_wes])
+        print("te recomendamos ver: \n", lista_western[peli_wes])
         pregu2 = input("¿Deseas otra recomendacion? ")
         if pregu2 != "no":
             peli_wes = peli_wes + 1
         else:
             peli_wes = len(lista_western)
+    print("\nNo tenemos más recomendaciones por el momento, gracias")
     
-if preferido =="Comedia":
+if preferido == ["Comedia"]:
     peli_com = 0
     while(peli_com < len(lista_comedia)):
-        print("te recomendamos ver: ", lista_comedia[peli_com])
+        print("te recomendamos ver: \n", lista_comedia[peli_com])
         pregu3 = input("¿Deseas otra recomendacion? ")
         if pregu3 != "no":
             peli_com = peli_com + 1
         else:
             peli_com = len(lista_comedia)
+    print("\nNo tenemos más recomendaciones por el momento, gracias")
         
-if preferido =="Caricatura":
+if preferido == ["Caricatura"]:
     peli_car = 0
     while(peli_car < len(lista_caricatura)):
-        print("te recomendamos ver: ", lista_caricatura[peli_car])
+        print("te recomendamos ver: \n", lista_caricatura[peli_car])
         pregu4 = input("¿Deseas otra recomendacion? ")
         if pregu4 != "no":
             peli_car = peli_car + 1
         else:
             peli_car = len(lista_caricatura)
+    print("\nNo tenemos más recomendaciones por el momento, gracias")
         
-if preferido =="Ficcion":
+if preferido == ["Ficcion"]:
     peli_fic = 0
     while(peli_fic < len(lista_ficcion)):
-        print("te recomendamos ver: ", lista_ficcion[peli_fic])
+        print("te recomendamos ver: \n", lista_ficcion[peli_fic])
         pregu5 = input("¿Deseas otra recomendacion? ")
         if pregu5 != "no":
             peli_fic = peli_fic + 1
         else:
             peli_fic = len(lista_ficcion)
+    print("\nNo tenemos más recomendaciones por el momento, gracias")
